3afc9e196899d2f3c8f8e1c176228ce31f9704dc
Brian Clovis <bwclovis.web@gmail.com>
2023-12-10 09:25:34 -0500

    update social nav

--- app/ui/molecules/Navigation/TopBarNavigation.tsx
+++ app/ui/molecules/Navigation/TopBarNavigation.tsx
@@ -1,12 +1,23 @@
 import CartPageLink from '../CartPageLink/CartPageLink'
+import { BsFacebook } from "react-icons/bs"
+import { TiSocialInstagram } from "react-icons/ti"
 import styles from './navigation.module.scss'
 
 const TopBarNavigation = () => (
   <div className={styles.topNav}>
     <ul>
-      <span>oh hai</span>
-      <CartPageLink />
+      <li>
+        <a href="https://www.facebook.com/groups/1259074817923302" target="_blank">
+          <BsFacebook size={30} />
+        </a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/happy_cat_tie_dye/" target="_blank">
+          <TiSocialInstagram size={38} />
+        </a>
+      </li>
     </ul>
+    <CartPageLink />
   </div>
 )
 
