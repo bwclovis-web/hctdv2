ed00894c063d19b1710124af0d77120bcba6a8be
Brian Clovis <bwclovis.web@gmail.com>
2023-12-23 09:02:30 -0500

    cart page updates

--- app/shop/cart/ui/CartDetails.tsx
+++ app/shop/cart/ui/CartDetails.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { useState, useRef, useEffect } from 'react'
+import { useState, useRef, useEffect, MouseEvent } from 'react'
 import { formatPrice } from '@/lib/utils/formatPrice'
 import styles from '../cartPage.module.scss'
-
+import clsx from 'clsx'
 
 const CartDetails = ({ data }) => {
   const [checked, setChecked] = useState(false)
@@ -20,18 +20,18 @@ const CartDetails = ({ data }) => {
     return <p>OHHAI</p>
   }
 
-  const handleCheckout = evt => {
+  const handleCheckout = (evt: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>) => {
     if (!checked) {
       evt.preventDefault()
       inputRef?.current?.focus()
       setError('Please agree by checking above.')
     }
-    // evt.preventDefault()
   }
 
-  const handleCheckbox = () => {
-    setChecked(!checked)
-  }
+  const CheckoutLinkClasses = clsx({
+    [styles.checkoutDisabled]: !checked,
+    [styles.checkoutEnabled]: checked
+  })
 
   return (
     <>
@@ -39,12 +39,12 @@ const CartDetails = ({ data }) => {
       <p>Sub Total: {formatPrice(data.subtotalPriceV2.amount, data.subtotalPriceV2.currencyCode)}</p>
       <p>By checking this I agree to the terms and conditions of the sale. </p>
       <div className={styles.termsCheckbox}>
-        <input type="checkbox" id="terms" checked={checked} onChange={() => handleCheckbox()} ref={inputRef} tabIndex={0} />
+        <input type="checkbox" id="terms" checked={checked} onChange={() => setChecked(!checked)} ref={inputRef} tabIndex={0} />
         <label htmlFor="terms" ><span></span>I Agree</label>
       </div>
       <div className={styles.checkoutContainer}>
         {error && <p>{error}</p>}
-        <a href={data.webUrl} onClick={evt => handleCheckout(evt)}>Oh hai</a>
+        <a href={data.webUrl} onClick={evt => handleCheckout(evt)} className={CheckoutLinkClasses}>Go to checkout</a>
       </div>
     </>
   )
